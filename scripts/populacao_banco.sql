b30269fd43811f40551fee3e6d92364cffc511e5
JulieneCarneiro <juliene.scarneiro@gmail.com>
2023-07-28 17:34:49 -0300

    adição de script de população do banco

---
+++ scripts/populacao_banco.sql
@@ -0,0 +1,23 @@
+USE `Biblioteca`;
+
+INSERT INTO `Enderecos` VALUES
+ (1,'Dos Bobos',0,'00000000','Casa Sem Nada'),
+ (2,'Torta',43,'33333333','Casa Torta'),
+ (3,'Se fosse minha',1000,'99999999','Ladrilhada Com Pedrinhas De Brilhantes'),
+ (4,'Do Ouvidor',1,'21000000','Machado de Assis'),
+ (5,'Cabeça de Porco',18,'21000001','Cortiço Do João Romão');
+ 
+ INSERT INTO `Editoras` VALUES 
+  (1,'Adeus Internacional',),
+  (2,'Livro de Folhas',),
+  (3,'Imprensa Solta Pendurada',),
+  (4,'A editora',),
+  (5,'Planeta Solitário',);
+ 
+ 
+ INSERT INTO `Pessoas` VALUES
+  (1,'Judith Flores','judith@flores.com','12345678910','1098754321','1945-04-18',4),
+  (2,'Sergio Malandro','hora@capeta.com','66666666666','9999999999','1955-10-12',1),
+  (3,'Cascão de Souza','cascao@turmadamonica.com','01010101010','11961611616','1961-08-01',5),
+  (4,'Senhor Torto','tortodesentortador@desentortando.com','43434343431','43434343432','1913-10-19',2),
+  (5,'Dona Creuza','creuzinhagatinha@bol.com','01123456789','98765432110','1980-08-03',3);
